9f79b0eda8b06122372b999a6cd12cbfaefab325
Sanassah <anas.senouci@gmail.com>
2023-07-19 13:52:50 -0400

    no message

--- main/ControlLawsScript.m
+++ main/ControlLawsScript.m
@@ -5,11 +5,14 @@ RPM=30/pi; %Rad/sec to RPM conversion
 %% Drone properties 
 m=3.4811482949175;      %Mass [kg]
 g=9.80665;              %Gravity [N/kg]
-L=0.4;
+L=0.4; %Arm length (Symetric config.)
 
 Jx=0.082298105960482;   %Inertia [Kg*m^2]
 Jy=0.082457201628499;
 Jz=0.15887270137511;
+
+k_T=1.34e-5; %Thrust coefficient of propellers
+k_y=1.28e-7; %Yaw coefficient of propellers
 
 bp=0.001; %rotating drag coefficient
 bq=0.001;
@@ -19,23 +22,24 @@ cx=5; %translation drag coefficient
 cy=5;
 cz=5;
 
-ts2=2; %Desired Position settling time
+ts2=1; %Desired Position settling time
 tsz=2; %Desired altitude settling time
-ts=2;  %Desired attitude settling time
+ts=ts2/2;  %Desired attitude settling time  
+tsy=ts; %Desired yaw settling time
 
-k_T=0.0000134; %Thrust coefficient of propellers
-k_y=2.28e-6; %Yaw coefficient 1.5925e-06;
 
-roll_angle_limit=pi/6; %angle (in Rad) to which the drone can incline
-pitch_angle_limit=pi/6;
-yaw_angle_limit=pi/6;
 
-max_rotorspeed=10000*2*pi/60; %Max rpm of rotors in Rad/s
-max_thrust=6*k_T*(max_rotorspeed)^2;
+roll_angle_limit=20/R2D; %angle (in Rad) to which the drone can incline
+pitch_angle_limit=20/R2D;
+yaw_angle_limit=20/R2D;
 
-max_pitch_moment=sqrt(3)*k_T*L*(max_rotorspeed)^2; %Max vehicle torque
-max_roll_moment=2*k_T*L*(max_rotorspeed)^2;
-max_yaw_moment=3*k_y*(max_rotorspeed)^2;
+max_rpm=10000/RPM; %Max rpm of rotors in Rad/s
+hov_rpm=6200/RPM; %prop speed while hovering in Rad/s
+hov_thrust=6*k_T*(hov_rpm)^2; %thrust while hovering
+max_thrust=6*k_T*(max_rpm)^2; %Max thrust
+max_pitch_moment=sqrt(3)*k_T*L*(max_rpm)^2; %Max vehicle torque
+max_roll_moment=2*k_T*L*(max_rpm)^2; 
+max_yaw_moment=k_y*hov_thrust/k_T;
 
 
 %% Attitude ctrl (inner loop 5X faster)
@@ -52,8 +56,8 @@ Kp=(Jx/Tc)-bp;
 Kp2= Jx*((4/ts))*((-4/ts)+((bp+Kp)/Jx))/Kp;
 
 %Yaw ctrl
-Kr=(Jz/Tc)-br;
-Kr2= Jz*((4/ts))*((-4/ts)+((br+Kr)/Jz))/Kr;
+Kr=(Jz/(tsy/25))-br;
+Kr2= Jz*((4/tsy))*((-4/tsy)+((br+Kr)/Jz))/Kr;
 
 %% Position ctrl (with zero placement)
 
@@ -84,8 +88,8 @@ l4=(p4+(4/ts2))/(cos(t4));
 lz=(-z_x+(4/ts2))/(cos(tz));
 
 
-%Kx2=l1*l2*l3*l4/((Kq*Kq2*g/Jy)*lz); %prod of pole lengths divided prod of zero lengths
-Kx= (p1+4/ts2)*(p2+4/ts2)*(p4+4/ts2)/(Kq*Kq2*g/Jy);
+Kx=l1*l2*l3*l4/((Kq*Kq2*g/Jy)*lz); %prod of pole lengths divided prod of zero lengths
+%Kx= (p1+4/ts2)*(p2+4/ts2)*(p4+4/ts2)/(Kq*Kq2*g/Jy);
 Kx=abs(Kx);
 
 
@@ -102,7 +106,7 @@ Pxs=[p1 p2 p3 p4];
 % X RL w/ zero
 sys = tf([c*g z_x*c*g],[1 (a+b) (a*b+c) (a*c) 0]);
 % X RL w zero
-%rlocus(sys); % X RL w zero
+rlocus(sys) 
 
 % Y ctrl
 
@@ -128,8 +132,8 @@ ly_3=(py_3+(4/ts2))/(cos(ty_3));
 ly_4=(py_4+(4/ts2))/(cos(ty_4));
 ly_z=(-z_y+(4/ts2))/(cos(ty_z));
 
-%Ky=ly_1*ly_2*ly_3*ly_4/((Kp*Kp2*g/Jx)*ly_z); 
-Ky= (py_1+4/ts2)*(py_2+4/ts2)*(py_4+4/ts2)/(Kp*Kp2*g/Jx);
+Ky=ly_1*ly_2*ly_3*ly_4/((Kp*Kp2*g/Jx)*ly_z); 
+%Ky= (py_1+4/ts2)*(py_2+4/ts2)*(py_4+4/ts2)/(Kp*Kp2*g/Jx);
 Ky=abs(Ky);
 
 %Root locus check
@@ -160,11 +164,11 @@ B=pinv(C_A);%Pseudoinverse of control allocation matrix
 % B * U = W_i^2
 
 %% Reliability of each rotors
-R1=0.5;
-R2=1;
+R1=1;
+R2=0.5;
 R3=1;
 R4=1;
-R5=1;
+R5=0.5;
 R6=1;
 
 
